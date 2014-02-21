0e7c3cd2a55c8025cf03274db0787366f11de3b3
mikeville <thedeal@gmail.com>
2014-02-21 16:08:46 -0500

    updated styles

--- javascripts/infastructure.js
+++ javascripts/infastructure.js
@@ -15,9 +15,10 @@ $(function(){
   window.canvas = d3.select("body")
               .append("svg")
               .attr("width", window.innerWidth)
-              .attr("height", window.innerHeight);
+              .attr("height", window.innerHeight)
 
   multiApiCall(year_data)
+
 
 })
 
--- javascripts/visualization.js
+++ javascripts/visualization.js
@@ -3,6 +3,7 @@ var addVisualization = function(){
   var dataset = all_articles.sort(dynamicSort("year"))
   var bar_height = window.innerHeight/80
   var margin_bottom = 50
+  var bar_padding = 1
 
   window.canvas.selectAll("rect")
         .data(dataset)
@@ -10,34 +11,35 @@ var addVisualization = function(){
         .append("rect")
         .on("mouseover", function(d,i) {
                                         d3.select(this)
-                                        .style("fill", "#333333")                                      
+                                        // .style("fill", "#333333")                                      
                                         $('#headline_container').html(d.data.original_article.forEach(function(article){
                                                                                                                 console.log(article)
                                                                                                                 }))
-                                        // $('#headline_container').html(d.data.words)
+                                        $('#headline_container').html(d.data.words)
+                                        $('#headline_container').css("color", "#ef4223")
 
 
                                         })
         .on("mouseout", function(d,i) {
                                         d3.select(this)
-                                        .style("fill", "#000000")
+                                        .style("fill", "#ef4223")
                                         })
         .attr("x", function(d, i){
           return i * (window.innerWidth / dataset.length) })
         .attr("y", function(d){
           return window.innerHeight - d.data.count*bar_height - margin_bottom})
         .attr("width", function(d){
-          return window.innerWidth/dataset.length
+          return window.innerWidth/dataset.length - bar_padding
         })
+        //ADDED PADDING
         .attr("height", 0)
-        .style("fill", "black")
+        .style("fill", "#ef4223")
         .transition()
         .attr("height", function(d){
           return d.data.count*bar_height
         })
         .duration(1000)
         .ease("bounce")
-
 
   window.canvas.selectAll("text")
         .data(dataset)
@@ -51,5 +53,8 @@ var addVisualization = function(){
         })
         .attr("y", function(d){
           return window.innerHeight - margin_bottom + 20})
+        .style("fill", "#ef4223")
+
+        d3.select("body").style("background-color", "#33150d");
         
 }
